65690e29793c14f7b938a8a371d17ce29748c23b
Yannis <Alopexer@163.com>
2024-10-04 14:42:21 +1000

    count book api

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
-<script setup>
-// import JSONLab from './components/JSONLab.vue'
-import BHeader from './components/BHeader.vue'
-// import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
+<script>
+import BHeader from './components/BHeader.vue';
+import CountBookAPI from './views/CountBookAPI.vue';
+
+export default {
+  name: 'App',
+  components: {
+    BHeader,
+    CountBookAPI
+  },
+  computed: {
+    showHeader() {
+      return this.$route.name !== 'CountBookAPI';
+    }
+  }
+};
 </script>
 
 <template>
   <div class="main-container">
-    <header>
+    <header v-if="showHeader">
       <BHeader />
     </header>
-
     <main class="main-box">
-      <!-- <LibraryRegistrationForm /> -->
-      <!-- <JSONLab /> -->
       <router-view></router-view>
     </main>
   </div>
